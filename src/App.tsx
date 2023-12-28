722cc310a854b3e2e74b4724d29087cabb7382a3
PokizaSdv <pokizasdv@gmail.com>
2023-12-28 01:38:11 -0500

    Implement updating status of task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { todoApi } from "./api/todo";
+import React, { useEffect, useState } from "react";
+import { todoApi } from "./api/todo"
 import "./App.css";
 
-const App: React.FC = () => {
+type TodoStatus = "TODO" | "INPROGRESS" | "DONE";
 
-    interface Todo {
-        id: number;
-        text: string;
-        status: string
-    }
+type Todo = {
+    id: string;
+    text: string;
+    status: TodoStatus;
+};
+
+const App: React.FC = () => {
     const [todos, setTodos] = useState<Todo[]>([]);
-    const [todoInputValue, setTodoInputValue] = useState("");
+    const [todoInputValue, setTodoInputValue] = useState<string>("");
 
     useEffect(() => {
         todoApi
@@ -23,15 +25,22 @@ const App: React.FC = () => {
             });
     }, []);
 
-    const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleTodoInputChange = (value: string): void => {
+        setTodoInputValue(value);
+    };
+
+    const createTodo = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (todoInputValue.length < 3) {
+            return;
+        }
 
         todoApi
-            .addTodo({ text: todoInputValue })
+            .create(todoInputValue)
             .then((response) => {
                 setTodoInputValue("");
                 setTodos((prevTodos) => {
-                    return [...prevTodos, ...response.data];
+                    return [...prevTodos, response.data];
                 });
             })
             .catch((err) => {
@@ -39,32 +48,67 @@ const App: React.FC = () => {
             });
     };
 
-    const handleTodoInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTodoInputValue(e.target.value);
+    const updateStatus = (id: string, status: TodoStatus): void => {
+        todoApi
+            .updateStatus(id, status)
+            .then((_) => {
+                setTodos((prevTodos) => {
+                    const copyTodos = prevTodos.map((todo) =>
+                        todo.id === id ? { ...todo, status } : todo
+                    );
+                    return copyTodos;
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
+
+    
 
     return (
         <main>
             <h1>Todo App</h1>
-            <form onSubmit={addTodo}>
-                <input
-                    placeholder="What is in your mind?"
-                    value={todoInputValue}
-                    onChange={handleTodoInputChange}
-                />
-                <button type="submit">Add Todo</button>
+
+            <form onSubmit={createTodo}>
+                <label>
+                    Todo:
+                    <input
+                        type="text"
+                        value={todoInputValue}
+                        onChange={(e) => handleTodoInputChange(e.target.value)}
+                    />
+                </label>
+                <button type="submit">
+                    Add Todo
+                </button>
             </form>
 
             <ul>
             {todos.map((todo) => {
-                    return (
-                        <li key={todo.id}>
-                            <span>{todo.text}</span>
-                            
-                        </li>
-                    );
-                })}
-            </ul>
+                return (
+                    <li key={todo.id}>
+                        <span>{todo.text}</span>
+                        <select
+                            value={todo.status}
+                            onChange={(e) =>
+                                updateStatus(
+                                    todo.id,
+                                    e.target.value as
+                                        | "TODO"
+                                        | "INPROGRESS"
+                                        | "DONE"
+                                )
+                            }
+                        >
+                            <option value={"TODO"}>Todo</option>
+                            <option value={"INPROGRESS"}>In Progress</option>
+                            <option value={"DONE"}>Done</option>
+                        </select>
+                    </li>
+                );
+            })}
+        </ul>
         </main>
     );
 };
--- src/api/todo.js
+++ src/api/todo.js
@@ -1,47 +1,63 @@
 class TodoAPI {
     constructor() {
-        this.endpoint = process.env.REACT_APP_API
+        this.endpoint = process.env.REACT_APP_API;
     }
-
-    async addTodo(todo) {
-        const {text} = todo;
-
+    async create(text) {
         try {
-            const response = await fetch(`${this.endpoint}/todos`, {
-                method: " POST",
-                headers: {"Content-type": "Application/json" },
-                body: JSON.stringify({text})
-            })
-            if(!response.ok) {
+            const response = await fetch(`${this.endpoint}/tasks`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    text: text,
+                }),
+            });
+
+            if (!response.ok) {
                 const data = await response.json();
-                throw new Error(data.message) 
+                throw new Error(data.message);
             }
-            return await response.json()
+
+            return await response.json();
         } catch (error) {
-            console.log(error)
+            console.log(error.message);
         }
     }
-
     async getAll() {
         try {
-            const response = await fetch(`${this.endpoint}/todos`);
-            if(!response.ok) {
-                const data = response.json();
-                throw new Error(data.message)
+            const response = await fetch(`${this.endpoint}/tasks`);
+            if (!response.ok) {
+                throw new Error(response.error);
             }
-            return await response.json()
-        } catch (error) { 
-            console.log(error)
-        }
-    }
 
-    async deleteOne(todoId) {
-        try {
-            
+            return await response.json();
         } catch (error) {
-            
+            console.log(error);
         }
     }
-} 
 
-export const todoApi = new TodoAPI()
+
+    async updateStatus(id, status) {
+        try {
+            const response = await fetch(`${this.endpoint}/tasks/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    status: status,
+                }),
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+
+export const todoApi = new TodoAPI();
